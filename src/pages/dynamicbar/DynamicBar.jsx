b1d44459fb68c5455bc94fd46dc164f61ed42407
Pravin <pravinavhad3597@gmail.com>
2025-01-19 12:08:44 +0530

    Performance Page Responsiveness

--- src/pages/dynamicbar/DynamicBar.jsx
+++ src/pages/dynamicbar/DynamicBar.jsx
@@ -17,14 +17,14 @@ const DynamicBar = () => {
             <div className="absolute inset-0 bg-gradient-to-t  from-black via-transparent to-black opacity-78 z-10 "></div>
             <div className="absolute inset-0 bg-gradient-to-l  from-black via-transparent to-black opacity-78 z-10 "></div>
 
-            <div className="absolute inset-0 z-10 flex items-center justify-center lg:px-12">
+            <div className="absolute inset-0 z-10 flex items-center flex-col lg:flex-row justify-center lg:px-12">
                 <div className="text-white lg:px-5">
-                    <h1 className="font-markot font-extrabold italic tracking-[0.1em] sm:tracking-[0.35em] mb-3 sm:mb-5 text-lg sm:text-xl lg:text-xl">
+                    <h1 className="font-markot font-extrabold italic tracking-[0.1em] sm:tracking-[0.35em] mb-3 sm:mb-5 text-base  lg:text-xl">
                     <h1 className="font-markot font-extrabold italic tracking-[0.35em] mb-2">
 							<span className="text-red-600">//</span>PERFORMANCE
 						</h1>
                     </h1>
-                    <h1 className="text-2xl  md:text-4xl lg:text-6xl font-extrabold font-clashdisplay">
+                    <h1 className="text-5xl  md:text-4xl lg:text-6xl font-extrabold font-clashdisplay">
                         INTUITIVE .<br /> SMART . <br /> 
                         <span className="text-red-600">
                             DYNAMIC 
@@ -44,7 +44,7 @@ const DynamicBar = () => {
                         alt="Glassmorphism"
                     />
                     <div className="relative flex flex-col justify-center lg:px-12 w-full h-[15rem]  text-white ">
-                        <p className="text-white font-markot text-sm sm:text-base md:text-lg lg:text-xl lg:mt-4  tracking-wide">
+                        <p className="text-white font-markot text-lg md:text-lg lg:text-xl lg:mt-4  tracking-wide">
                             With <span className="text-itel-red font-bold">Dynamic Bar,</span>{" "}
                             get notified calls, and alerts in an unobtrusive way, so you never
                             miss what matters while enjoying everything that matters.
