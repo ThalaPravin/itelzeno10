8f5b3b2773e37a7cbab0e132ad97e77e4fb7c644
Pravin <pravinavhad3597@gmail.com>
2025-01-19 12:45:10 +0530

    Text Tracking issue resolved

--- src/pages/battery/Battery.jsx
+++ src/pages/battery/Battery.jsx
@@ -7,58 +7,59 @@ import Stripe from "./components/Stripe.jsx";
 const Battery = () => {
   return (
     <>
+      <div className="relative w-screen h-screen bg-black">
+        <div className="absolute inset-0 bg-gradient-to-bl  from-black via-transparent to-black opacity-78 z-10 "></div>
 
-    <div className="relative w-screen h-screen bg-black">
-
-       <div className="absolute inset-0 bg-gradient-to-bl  from-black via-transparent to-black opacity-78 z-10 "></div>
-   
-      <div className="absolute inset-0 z-0 flex justify-between items-end">
-        <div>
-          <img src={zenoicon} alt="Zeno Icon" className="h-36 sm:h-48 lg:h-[18rem]" />
+        <div className="absolute inset-0 z-0 flex justify-between items-end">
+          <div>
+            <img
+              src={zenoicon}
+              alt="Zeno Icon"
+              className="h-36 sm:h-48 lg:h-[18rem]"
+            />
+          </div>
+          <div>
+            <img
+              src={battery}
+              alt="Battery"
+              className="w-40 sm:w-60 md:w-80 lg:w-[32rem]"
+            />
+          </div>
         </div>
-        <div>
-          <img
-            src={battery}
-            alt="Battery"
-            className="w-40 sm:w-60 md:w-80 lg:w-[32rem]"
-          />
-        </div>
-      </div>
 
-    
-      <div className="absolute inset-0 z-10 flex items-center justify-start">
-        <div className="w-full sm:w-3/4 md:w-1/2 px-5 sm:px-10 md:ml-20 relative">
-          <img
-            className="absolute inset-0 bg-cover bg-center backdrop-blur-lg"
-            src={glassmorphism}
-            alt="Glassmorphism"
-          />
-          <div className="relative flex flex-col justify-center px-5 sm:px-12 lg:px-24 mt-6 sm:mt-9 w-full h-[15rem] sm:h-[17.625rem] text-white">
-          <h1 className="font-markot font-extrabold italic tracking-[0.1em] sm:tracking-[0.35em] mb-3 sm:mb-5 text-lg sm:text-xl lg:text-xl font-markot font-extrabold italic tracking-[0.35em] mb-2">
-                   
-							<span className="text-red-600">//</span>PERFORMANCE
-					
-                    </h1>
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold font-clashdisplay">
-              NON STOP POWER, <br />
-              <span className="text-red-600">
-                INSPIRED BY YOU <br />
-              </span>
-            </h1>
-            <p className="text-white font-markot text-sm sm:text-base md:text-lg lg:text-xl mt-4 sm:mt-6 tracking-wide">
-              Designed for those who live life on the go, Zeno is packed with a
-              massive
-              <span className="text-itel-red font-bold"> 5000mAh battery</span>,
-              ensuring you stay connected, entertained, and productive all day
-              long.
-            </p>
+        <div className="absolute inset-0 z-10 flex items-center justify-start">
+          <div className="w-full sm:w-3/4 md:w-1/2 px-5 sm:px-10 md:ml-20 relative">
+            <img
+              className="absolute inset-0 bg-cover bg-center backdrop-blur-lg"
+              src={glassmorphism}
+              alt="Glassmorphism"
+            />
+            <div className="relative flex flex-col justify-center px-5 sm:px-12 lg:px-24 mt-6 sm:mt-9 w-full h-[15rem] sm:h-[17.625rem] text-white">
+              <h1 className="font-markot font-extrabold italic   mb-3 sm:mb-5 text-lg sm:text-xl lg:text-lg font-markot font-extrabold italic tracking-[0.3em] mb-2">
+                <span className="text-red-600">//</span>BATTERY
+              </h1>
+              <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold font-clashdisplay">
+                NON STOP POWER, <br />
+                <span className="text-red-600">
+                  INSPIRED BY YOU <br />
+                </span>
+              </h1>
+              <p className="text-white font-markot text-sm sm:text-base md:text-lg lg:text-xl mt-4 sm:mt-6 tracking-wide">
+                Designed for those who live life on the go, Zeno is packed with
+                a massive
+                <span className="text-itel-red font-bold">
+                  {" "}
+                  5000mAh battery
+                </span>
+                , ensuring you stay connected, entertained, and productive all
+                day long.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <Stripe />
-
+      <Stripe />
     </>
   );
 };
--- src/pages/storage/Storage.jsx
+++ src/pages/storage/Storage.jsx
@@ -1,47 +1,44 @@
-import React from 'react';
-import backgroundImage from '../../assets/Zeno_10/storage/bg.jpg';
+import React from "react";
+import backgroundImage from "../../assets/Zeno_10/storage/bg.jpg";
 import glassmorphism from "../../assets/Zeno_10/battery/Glassmorphism.png";
 
-
 const Storage = () => {
-    return (
-        <div className="relative w-screen h-screen bg-black">
+  return (
+    <div className="relative w-screen h-screen bg-black">
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      ></div>
 
-        <div
-            className="absolute inset-0 bg-cover bg-center"
-            style={{ backgroundImage: `url(${backgroundImage})` }}
-        ></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-78 z-10 "></div>
 
-        <div className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-black opacity-78 z-10 "></div>
-
-            <div className="absolute inset-0 z-10 flex items-center justify-start ">
-                <div className="w-full sm:w-3/4 md:w-1/2 px-5 md:px-0 md:ml-20 relative mx-2">
-                    <img
-                        className="absolute inset-0 bg-cover bg-center backdrop-blur-xl"
-                        src={glassmorphism}
-                        alt="Glassmorphism"
-                    />
-                    <div className="relative flex flex-col justify-center px-5 px-0 mt-0 lg:px-24 md:px-0  sm:mt-9 w-full h-[15rem]  text-white lg:mt-14">
-                    <h1 className="font-markot font-extrabold italic tracking-[0.1em] sm:tracking-[0.35em] mb-3 sm:mb-5 text-lg sm:text-xl lg:text-xl">
-                    <h1 className="font-markot font-extrabold italic tracking-[0.35em] mb-2">
-							<span className="text-red-600">//</span>STORAGE
-						</h1>
-                    </h1>
-                        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold font-clashdisplay">
-                            FUTURE READY  <br />
-                            <span className="text-red-600">
-                                POWER HOUSE <br />
-                            </span>
-                        </h1>
-                        <p className="text-white font-markot text-sm sm:text-base md:text-lg lg:text-xl lg:mt-4  tracking-wide">
-                        Whether you're editing videos, gaming in high-definition, or running demanding applications, Zeno delivers seamless performance every single time.
-                        
-                        </p>
-                    </div>
-                </div>
-            </div>
+      <div className="absolute inset-0 z-10 flex items-center justify-start ">
+        <div className="w-full sm:w-3/4 md:w-1/2 px-5 md:px-0 md:ml-20 relative mx-2">
+          <img
+            className="absolute inset-0 bg-cover bg-center backdrop-blur-xl"
+            src={glassmorphism}
+            alt="Glassmorphism"
+          />
+          <div className="relative flex flex-col justify-center px-5 px-0 mt-0 lg:px-24 md:px-0  sm:mt-9 w-full h-[15rem]  text-white lg:mt-14">
+            <h1 className="font-markot font-extrabold italic   mb-3 sm:mb-5 text-lg sm:text-xl lg:text-lg font-markot font-extrabold italic tracking-[0.3em] mb-2">
+              <span className="text-red-600">//</span>STORAGE
+            </h1>
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-extrabold font-clashdisplay">
+              FUTURE READY <br />
+              <span className="text-red-600">
+                POWER HOUSE <br />
+              </span>
+            </h1>
+            <p className="text-white font-markot text-sm sm:text-base md:text-lg lg:text-xl lg:mt-4  tracking-wide">
+              Whether you're editing videos, gaming in high-definition, or
+              running demanding applications, Zeno delivers seamless performance
+              every single time.
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Storage;
